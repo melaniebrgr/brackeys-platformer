c38ba5e44e3776480214baa91157465473676227
Melanie Burger <melanie.brgr@gmail.com>
2025-04-29 08:29:40 +0200

    feat: :sparkles: slimes back and forth

---
+++ scripts/slime.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+const SPEED = 60
+var direction = 1
+@onready var ray_cast_right = $RayCast2DRight
+@onready var ray_cast_left = $RayCast2DLeft
+@onready var animated_sprite_2D = $AnimatedSprite2D
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite_2D.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite_2D.flip_h = false
+	position.x += direction * SPEED * delta
