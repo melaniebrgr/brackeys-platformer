a9454f806120a5fd8320d3a25c29ac17f03d4417
Melanie Burger <melanie.brgr@gmail.com>
2025-04-29 08:41:07 +0200

    feat: :ambulance: free falls on death

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -6,6 +6,7 @@ var direction = 1
 @onready var ray_cast_left = $RayCast2DLeft
 @onready var animated_sprite_2D = $AnimatedSprite2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
